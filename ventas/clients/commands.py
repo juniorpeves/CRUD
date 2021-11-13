2692aa9d2657e252304df5d098bcb4b19144ae01
juniorpeves <juniorpeves@gmail.com>
2021-11-13 17:45:20 -0500

    Comentarios agregados a carpeta ventas

--- ventas/clients/commands.py
+++ ventas/clients/commands.py
@@ -1,34 +1,36 @@
 import click
 
+#ctx es el diccionario vacio creado en pv.py
+#Decorador para que clients sea un grupo(otro decorador)
 @click.group()
 def clients():
     """Manages the clients lifecycle"""
     pass
 
 
-@clients.command()
-@click.pass_context
+@clients.command() #Con esto ya es comando de clients 
+@click.pass_context #Pasar el contexto
 def create(ctx, name, company, email, position):
     """Creates a ew client"""
     pass
 
 
-@clients.command()
-@click.pass_context
+@clients.command() #Con esto ya es comando de clients 
+@click.pass_context #Pasar el contexto
 def list(ctx):
     """List all clients"""
     pass
 
 
-@clients.command()
-@click.pass_context
+@clients.command() #Con esto ya es comando de clients 
+@click.pass_context #Pasar el contexto
 def update(ctx, client_uid):
     """Update a client"""
     pass
 
 
-@clients.command()
-@click.pass_context    
+@clients.command() #Con esto ya es comando de clients 
+@click.pass_context #Pasar el contexto  
 def delete(ctx, client_uid):
     """Delete a client"""
     pass
--- ventas/pv.py
+++ ventas/pv.py
@@ -1,10 +1,12 @@
 import click
-
-from clients import commands as clients_commands
+from clients import commands as clients_commands 
+"""Importando los comandos asignando nuevo nombre"""
 
 @click.group()
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+#ctx se inicia como diccionario vacio
 
+"""Registrando todos los comandos"""
 cli.add_command(clients_commands.all)
