dd4caa11fe405fe097b13da650d1e7c47f7ace61
juniorpeves <juniorpeves@gmail.com>
2021-11-13 18:15:33 -0500

    edit models y services

---
+++ ventas/clients/models.py
@@ -0,0 +1,18 @@
+import uuid
+
+class Client:
+    
+    def __init__(self, name, company, email, position, uid=None):
+        self.name = name
+        self.company = company
+        self.email = email
+        self.position = position
+        self.uid = uid or uuid.uuid4() #Usa uuid para obtener un id unico 
+        
+    def to_dict(self):
+        return vars(self) #Nos permite acceder a la representación de diccionario
+    
+    @staticmethod #Representación columnar de los datos
+    def schema():
+        return ['name', 'company', 'email', 'position', 'uid']
+    
