33912a639be03abd0cbeb7171d7cfe1af7761dfe
juniorpeves <juniorpeves@gmail.com>
2021-11-13 17:46:01 -0500

    Comentarios en carpeta ventas

--- ventas/setup.py
+++ ventas/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+"""Importar el modulo setup"""
 
 setup(
     name='pv',
@@ -12,3 +13,8 @@ setup(
         pv=pv:cli
     ''',
 )
+# Nombre de la aplicacion pv
+# Versiones la 0.1
+# El modulo se llamada pv
+# Requisito el modulo click
+# El punto de entrada es el metodo cli (Declarado en pv.py)
