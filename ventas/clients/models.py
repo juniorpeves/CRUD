504e75b72a47340ad3f744cf50df465f4923d113
juniorpeves <juniorpeves@gmail.com>
2021-11-14 12:47:19 -0500

    Comentarios en clients

--- ventas/clients/models.py
+++ ventas/clients/models.py
@@ -1,13 +1,13 @@
 import uuid
 
 class Client:
-    
+    #Inicializando el cliente
     def __init__(self, name, company, email, position, uid=None):
         self.name = name
         self.company = company
         self.email = email
         self.position = position
-        self.uid = uid or uuid.uuid4() #Usa uuid para obtener un id unico 
+        self.uid = uid or uuid.uuid4() #Usa uuid para obtener un id unico (Estandarizado)
         
     def to_dict(self):
         return vars(self) #Nos permite acceder a la representación de diccionario
--- ventas/clients/services.py
+++ ventas/clients/services.py
@@ -1,5 +1,5 @@
 import csv
-
+#Directiva de importación para  usar la función sin llamar el modulo
 from clients.models import Client
 
 class ClientService:
@@ -8,6 +8,9 @@ class ClientService:
         self.table_name = table_name
     
     def create_client(self, client): 
+        #Abriendo el archivo
         with open(self.table_name, mode='a') as f:
+            # Inicializando el csv con el schema de Client
             writer = csv.DictWriter(f, fieldnames=Client.schema())
+            # Agregando al cliente nuevo en una nueva fila como diccionario
             writer.writerow(client.to_dict()) 
