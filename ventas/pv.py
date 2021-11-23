27bb2cce27d8126a2bfe907d04263499bf1c0ddf
juniorpeves <juniorpeves@gmail.com>
2021-11-22 21:15:11 -0500

    Clase41_click.option&create_client

--- ventas/pv.py
+++ ventas/pv.py
@@ -2,10 +2,13 @@ import click
 from clients import commands as clients_commands 
 """Importando los comandos asignando nuevo nombre"""
 
+CLIENT_TABLE = 'clients.csv'
+
 @click.group()
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+    ctx.obj['clients_table'] = CLIENT_TABLE
 #ctx se inicia como diccionario vacio
 
 """Registrando todos los comandos"""
