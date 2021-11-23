0d970fd3eb4f1a840917db97ba4c090d2045438e
juniorpeves <juniorpeves@gmail.com>
2021-11-22 21:14:41 -0500

    Clase41_click.option+create_client

--- ventas/clients/commands.py
+++ ventas/clients/commands.py
@@ -1,4 +1,7 @@
 import click
+from click.termui import prompt
+from clients.services import ClientService
+from clients.models import Client
 
 #ctx es el diccionario vacio creado en pv.py
 #Decorador para que clients sea un grupo(otro decorador)
@@ -9,11 +12,33 @@ def clients():
 
 
 @clients.command() #Con esto ya es comando de clients 
+#   CLASE41: 
+#   Click ayuda a perdir al usuario crenado la opción
+#   Shortname -n y Longname --name
+@click.option('-n','--name',
+              type=str,
+              prompt=True,
+              help='The client name')
+@click.option('-c','--company',
+              type=str,
+              prompt=True,
+              help='The client company')
+@click.option('-e','--email',
+              type=str,
+              prompt=True,
+              help='The client email')
+@click.option('-p','--position',
+              type=str,
+              prompt=True,
+              help='The client position')
 @click.pass_context #Pasar el contexto
 def create(ctx, name, company, email, position):
-    """Creates a ew client"""
-    pass
-
+    """Creates a new client"""
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+    
+    client_service.create_client(client)
+    
 
 @clients.command() #Con esto ya es comando de clients 
 @click.pass_context #Pasar el contexto
