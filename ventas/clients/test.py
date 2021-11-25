1f399e6da8b068bafa1b06b7b64ed6381980fbaa
juniorpeves <juniorpeves@gmail.com>
2021-11-24 21:24:06 -0500

    CLASE45_ManejoDeErrores

--- ventas/clients/test.py
+++ ventas/clients/test.py
@@ -1,4 +1,16 @@
+"""
 list = [{'name': 'Junior', 'company': 'BITEL', 'email': 'juniorpeves@gmail.com', 'position': 'Supervisor', 'uid': '6aaba566-64c0-4002-b39e-efa7bdebd4dc'}, 
         {'name': 'Carlos', 'company': 'MDM', 'email': 'carlospeves@gmail.com', 'position': 'Admin', 'uid': '3bfb4e89-b2fe-4067-adc7-4cd832fac752'}]
-print(list[0])
-print(type(list[0]))
+borrar=[list[0]]
+list.remove(borrar[0])
+print(list)
+"""
+def divide(denominator):
+        if denominator == 0:
+                raise ZeroDivisionError("No se puede dividir entre 0")
+        else:
+                print(denominator)
+                
+if __name__ == '__main__':
+        divide(0)
+
